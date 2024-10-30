e2ad85fc440b504c4cea4b04ba3c5927582ce1a7
HornVanhong <149969011+HornVanhong@users.noreply.github.com>
2024-10-30 11:08:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function loadPage(page) {
-    fetch(page)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Page not found');
-            }
-            return response.text();
-        })
-        .then(content => {
-            document.getElementById("content").innerHTML = content;
-            
-            // Load specific CSS for the About page
-            if (page === 'Page/about.html') {
-                const aboutStyle = document.createElement('link');
-                aboutStyle.rel = 'stylesheet';
-                aboutStyle.href = 'Page/about.css';
-                document.head.appendChild(aboutStyle);
-            }
-        })
-        .catch(error => console.error('Error loading page:', error));
+  fetch(page)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Page not found");
+      }
+      return response.text();
+    })
+    .then((content) => {
+      document.getElementById("content").innerHTML = content;
+
+      // Load specific CSS for the About page
+      if (page === "./Page/about.html") {
+        const aboutStyle = document.createElement("link");
+        aboutStyle.rel = "stylesheet";
+        aboutStyle.href = "Page/about.css";
+        document.head.appendChild(aboutStyle);
+      }
+    })
+    .catch((error) => console.error("Error loading page:", error));
 }
